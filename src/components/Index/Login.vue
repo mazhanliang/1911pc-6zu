31283e1abe1db7d032e65767e22e4715ff23af93
mzl <2652592612@qq.com>
2020-08-15 15:50:20 +0800

    模板布局

---
+++ src/components/Index/Login.vue
@@ -0,0 +1,88 @@
+<template>
+
+  <div class="micronews micronews-login" >
+    <my-header></my-header>
+
+    <div class="micronews-login-container">
+      <div class="form-box">
+        <h3>登录</h3>
+        <div class="wrap">
+          <form class="layui-form"> <!-- 提示：如果你不想用form，你可以换成div等任何一个普通元素 -->
+            <div class="layui-form-item">
+              <div class="layui-input-block">
+                <input type="text" name="" lay-verify="required|phone" id="phone" placeholder="请输入手机号" autocomplete="off" class="layui-input">
+              </div>
+            </div>
+            <div class="layui-form-item">
+              <div class="layui-input-block">
+                <input type="text"  name="" lay-verify="required" id="imgCode" placeholder="验证码" autocomplete="off" class="layui-input">
+                <img src="https://fly.layui.com/auth/imagecode?t=1542856673772">
+              </div>
+            </div>
+            <div class="layui-form-item">
+              <div class="layui-input-block">
+                <input type="text"  name="" lay-verify="required" placeholder="请输入短信验证码" autocomplete="off" class="layui-input">
+                <input type="button"  id="veriCodeBtn" name="" value="验证码" class="obtain layui-btn">
+              </div>
+            </div>
+            <div class="layui-form-item agreement">
+              <div class="layui-input-block">
+                <input type="checkbox" name="like1[write]" lay-verify="required" lay-skin="primary" title="我已阅读并同意" checked="">
+                <span class="txt"><a href="#">用户协议</a>和<a  href="#">隐私条款</a></span>
+
+              </div>
+            </div>
+            <div class="layui-form-item">
+              <div class="layui-input-block">
+                <button class="layui-btn" lay-submit lay-filter="*" onclick="return false">登录</button>
+              </div>
+            </div>
+            <!-- 更多表单结构排版请移步文档左侧【页面元素-表单】一项阅览 -->
+          </form>
+          <div class="other-login">
+            <a href="#">
+              <img src="@/assets/res/static/images/load1.png">
+            </a>
+            <a href="#">
+              <img src="@/assets/res/static/images/load2.png">
+            </a>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  import MyHeader from '@/components/Index/Head'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  },
+  components:{
+    MyHeader:MyHeader,
+
+  },
+  methods:{
+
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Index from "@/components/Index/Index";
+import List from "@/components/Index/List";
+import Details from "@/components/Index/Details";
+import Login from "@/components/Index/Login";
+import PersInfo from "@/components/Index/PersInfo";
+import Search from "@/components/Index/Search";
 
 Vue.use(Router)
 
@@ -8,8 +14,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
+    },
+    {
+      path: '/details',
+      name: 'Details',
+      component: Details
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/persInfo',
+      name: 'PersInfo',
+      component: PersInfo
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    },
   ]
 })
